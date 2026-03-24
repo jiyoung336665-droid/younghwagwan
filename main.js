0a3a4208c4b398e74927002ce746c71ea497a267
jiyoung336665 <jiyoung336665@gmail.com>
2026-03-24 11:24:52 +0000

    Update: Rename to High Layered Cut and improve description readability

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const hairstyles = [
     },
     {
         id: 2,
-        name: '레이어드 컷',
+        name: '하이 레이어드 컷',
         category: 'long',
-        description: '가벼운 층감으로 얼굴형을 보완하고 세련된 무드를 연출하는 스타일',
+        description: '가벼운 층감으로 얼굴형을 보완하고<br>세련된 무드를 연출하는 스타일',
         imageUrl: 'images/style2.jpg'
     },
     {
